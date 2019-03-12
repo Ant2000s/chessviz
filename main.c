33392f77e03ac88ada15629c57a85317c45c6c0a
anton <skrebnev.anton@gmail.com>
2019-03-12 12:17:05 +0700

    change main.c (with clang-format)

--- main.c
+++ main.c
@@ -9,23 +9,22 @@ void board(char board[8][8])
         for (int j = 0; j < 8; j++) {
             printf("%c |", board[i][j]);
         }
-    printf("\n");
+        printf("\n");
     }
-    printf("  _______________\n");
-    printf("  A B C D E F G H\n");
+    printf("  _______________________\n");
+    printf("  A  B  C  D  E  F  G  H \n");
 }
 
 int main()
 {
-	char dos[8][8] = {
- 		"rhbqkbhr",
- 		"pppppppp",
- 		"        ",
- 		"        ",
- 		"        ",
- 		"        ",
- 		"PPPPPPPP",
- 		"RHBQKBHR" };
+    char dos[8][8] = {"rhbqkbhr",
+                      "pppppppp",
+                      "        ",
+                      "        ",
+                      "        ",
+                      "        ",
+                      "PPPPPPPP",
+                      "RHBQKBHR"};
     board(dos);
     return 0;
 }
