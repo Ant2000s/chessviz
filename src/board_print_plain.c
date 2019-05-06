bb7143acd0e04ac03a1e5e390a42426fb1dfbef0
anton <skrebnev.anton@gmail.com>
2019-05-06 21:30:44 +0700

    bugfix and clang-format

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -1,30 +1,18 @@
+#include "board_print_plain.h"
 #include <stdio.h>
 
-void board(char board[8][8])
+extern char desk[8][8];
+void board()
 {
     printf("\n");
     for (int i = 7; i >= 0; i--) {
         printf("—+——+——+——+——+——+——+——+——+\n");
         printf("%d|", i + 1);
         for (int j = 0; j < 8; j++) {
-            printf("%c |", board[i][j]);
+            printf("%c |", desk[i][j]);
         }
         printf("\n");
     }
     printf("  _______________________\n");
     printf("  A  B  C  D  E  F  G  H \n");
-}
-
-int main()
-{
-    char dos[8][8] = {"rhbqkbhr",
-                      "pppppppp",
-                      "        ",
-                      "        ",
-                      "        ",
-                      "        ",
-                      "PPPPPPPP",
-                      "RHBQKBHR"};
-    board(dos);
-    return 0;
 }
