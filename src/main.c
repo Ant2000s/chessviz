8d7ecc122cdb915f0e61db4153c1c2ab4a915fa0
anton <skrebnev.anton@gmail.com>
2019-05-07 12:42:06 +0700

    add new function(pawn)

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "board_print_plain.h"
+#include "board.h"
 #include <stdio.h>
 
 // extern char desk[8][8];
@@ -15,5 +16,13 @@ char desk[8][8] = {"rhbqkbhr",
 int main()
 {
     board();
+    for (;;) {
+     start(1);
+     move();
+     board();
+     start(2);
+     move(2);
+     board();
+     }
     return 0;
 }
