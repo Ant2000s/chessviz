ad3a396bed335c6bcbe3d2a92a6205926d226ecd
anton <skrebnev.anton@gmail.com>
2019-05-13 22:44:51 +0700

    added message display; clang-format

--- src/main.c
+++ src/main.c
@@ -1,11 +1,10 @@
-#include "board_print_plain.h"
 #include "board.h"
+#include "board_print_plain.h"
 #include <stdio.h>
 
 // extern char desk[8][8];
 
-char desk[8][8] = {
-                   "RHBQKBHR",
+char desk[8][8] = {"RHBQKBHR",
                    "PPPPPPPP",
                    "        ",
                    "        ",
@@ -18,12 +17,14 @@ int main()
 {
     board();
     for (;;) {
-     start(1);
-     move();
-     board();
-     start(2);
-     move();
-     board();
-     }
+        printf("Введите действие белых: ");
+        start(1);
+        move();
+        board();
+        printf("Введите действие чёрных: ");
+        start(2);
+        move();
+        board();
+    }
     return 0;
 }
