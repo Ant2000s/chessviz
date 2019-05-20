252c043d5b4f89a965109a1dbe94e2287a26519d
anton <skrebnev.anton@gmail.com>
2019-05-20 22:50:20 +0700

    new functions

--- src/board.h
+++ src/board.h
@@ -1,8 +1,14 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-void start(int flag);
+void start();
+int checkWin();
 int white();
 int black();
 void move();
+int checkY();
+int checkX();
+int checkD();
+void transformPawn();
+
 #endif
